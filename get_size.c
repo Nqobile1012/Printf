c4b5314293e096487bd135777aed855f10a319a6
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-09-19 06:41:48 -0700

    Shenge

---
+++ get_size.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_size - Gets the size to cast the argument
+ * @format: Formatted string
+ * @i: list of args to the printed
+ * Return: precision
+ */
+
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+	return (size);
+}
